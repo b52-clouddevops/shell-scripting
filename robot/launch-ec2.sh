56b66dafd48202bea661c4f1dc3ebec00ddaf6d1
CodingManoj <mvp2612@hotmail.com>
2023-01-05 06:51:36 +0530

    wefdsx

--- robot/launch-ec2.sh
+++ robot/launch-ec2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+# This script creates the server and the DNS Record
 
 COMPONENT=$1 
 HOSTED_ZONE_ID="Z090521761DHPU3HXLNP"
@@ -9,15 +10,29 @@ SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b52-al
 
 echo -e "AMI ID Used to launch the instance is \e[32m $AMI_ID \e[0m "
 echo -e "Security Group ID Used to launch the instance is \e[32m  $SG_ID \e[0m"
-echo "******______ $COMPONENT launch is in progress ______****** \n"
+echo "******______ $COMPONENT launch is in progress ______******"
 
-PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+launch_ec2() {
 
-echo -e "Private_ip of the $COMPONENT Server is \e[32m $PRIVATE_IP \e[0m"
+    PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-echo -n "Creating Internal DNS Record for $COMPONENT" 
+    echo -e "Private_ip of the $COMPONENT Server is \e[32m $PRIVATE_IP \e[0m"
 
-sed -e "s/IPADDRESS/$PRIVATE_IP/" -e "s/COMPONENT/$COMPONENT/" route53.json  > /tmp/r53.json 
-aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch file:///tmp/r53.json 
+    echo -n "Creating Internal DNS Record for $COMPONENT" 
 
-echo -n "\n ______****** Internal DNS Record for $COMPONENT is completed ******__________" 
+    sed -e "s/IPADDRESS/$PRIVATE_IP/" -e "s/COMPONENT/$COMPONENT/" route53.json  > /tmp/r53.json 
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch file:///tmp/r53.json 
+
+    echo -n " ______****** Internal DNS Record for $COMPONENT is completed ******__________"  
+
+}
+
+# If the selected option to launch is all, it's going to create all the servers.
+if [ "$1" == "all" ]; then 
+    for component in frontend catalogue cart user shipping payment mysql rabbitmq redis mongodb; do 
+        COMPONENT=$component
+        launch_ec2                  # Calling Create Server Function
+    done 
+else 
+        launch_ec2                  # Calling Create Server Function
+fi 
