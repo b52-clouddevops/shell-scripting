4e2efc7ce98a0a50fb7d8cc09875a8478e56abd4
CodingManoj <mvp2612@hotmail.com>
2023-01-05 06:25:43 +0530

    wefdsx

--- robot/launch-ec2.sh
+++ robot/launch-ec2.sh
@@ -6,9 +6,13 @@ COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7" --region us-east-1 | jq .Images[].ImageId | sed -e 's/"//g')
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b52-allow-all --region us-east-1 | jq .SecurityGroups[].GroupId | sed -e 's/"//g')
 
-echo "AMI ID Used to launch the instance is $AMI_ID "
-echo "Security Group ID Used to launch the instance is $SG_ID "
+echo -e "AMI ID Used to launch the instance is \e[32m $AMI_ID \e[0m "
+echo -e "Security Group ID Used to launch the instance is \e[32m  $SG_ID \e[0m"
 echo "******______ $COMPONENT launch is in progress ______******"
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g'
+PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+echo -e "Private_ip of the $COMPONENT Server is \e[32m $PRIVATE_IP \e[0m"
+
+echo  "******______ $COMPONENT launch is Completed ______******"
 
