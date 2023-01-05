b3d3b855926f65ad7d2382ba2d8658d343cb6846
CodingManoj <mvp2612@hotmail.com>
2023-01-05 11:09:34 +0530

    codePushByCodingManohjAutoScript

--- robot/launch-ec2.sh
+++ robot/launch-ec2.sh
@@ -10,7 +10,6 @@ if [ -z "$COMPONENT" ] || [ -z "$ENV" ]; then
     exit 1
 fi 
 
-# AMI_ID="ami-00ff427d936335825"
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7" --region us-east-1 | jq .Images[].ImageId | sed -e 's/"//g')
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b52-allow-all --region us-east-1 | jq .SecurityGroups[].GroupId | sed -e 's/"//g')
 
