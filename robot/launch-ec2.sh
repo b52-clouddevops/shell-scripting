420b065d5eb6870ee0fa9e7a45f0edcde1857e83
CodingManoj <mvp2612@hotmail.com>
2023-01-05 06:38:25 +0530

    wefdsx

--- robot/launch-ec2.sh
+++ robot/launch-ec2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
 COMPONENT=$1 
+HOSTED_ZONE_ID="Z090521761DHPU3HXLNP"
 
 # AMI_ID="ami-00ff427d936335825"
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7" --region us-east-1 | jq .Images[].ImageId | sed -e 's/"//g')
@@ -14,5 +15,9 @@ PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro
 
 echo -e "Private_ip of the $COMPONENT Server is \e[32m $PRIVATE_IP \e[0m"
 
-echo  "******______ $COMPONENT launch is Completed ______******"
+echo -n "Creating Internal DNS Record for $COMPONENT" 
 
+sed -e "s/IPADDRESS/$PRIVATE_IP/" -e "s/COMPONENT/$COMPONENT" route53.json  > /tmp/r53.json 
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch file:///tmp/r53.json 
+
+echo -n "______****** Internal DNS Record for $COMPONENT is completed ******__________" 
